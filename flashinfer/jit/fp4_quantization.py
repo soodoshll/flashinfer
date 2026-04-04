60aee94a6fa3127f03c8926e18bc31a8c82fadfb
root <yunzheq@nvidia.com>
2026-04-04 04:06:28 +0000

    update flag

--- flashinfer/jit/fp4_quantization.py
+++ flashinfer/jit/fp4_quantization.py
@@ -20,7 +20,9 @@ from .core import JitSpec, gen_jit_spec
 from . import env as jit_env
 from .core import (
     sm100a_nvcc_flags,
+    sm100f_nvcc_flags,
     sm103a_nvcc_flags,
+    sm107a_nvcc_flags,
     sm90a_nvcc_flags,
     sm110a_nvcc_flags,
     sm120a_nvcc_flags,
@@ -67,6 +69,14 @@ def gen_fp4_quantization_sm100_module() -> JitSpec:
 
 def gen_fp4_quantization_sm103_module() -> JitSpec:
     return gen_fp4_quantization_module(sm103a_nvcc_flags, "103")
+
+
+def gen_fp4_quantization_sm107_module(map_sm107_to_100f: bool = True) -> JitSpec:
+    # SM107 (Rubin) maps to sm100f while the CUTLASS submodule doesn't
+    # recognise compute_107a (TMA macros would not be defined).
+    # Set map_sm107_to_100f=False once CUTLASS adds SM107 support.
+    nvcc_flags = sm100f_nvcc_flags if map_sm107_to_100f else sm107a_nvcc_flags
+    return gen_fp4_quantization_module(nvcc_flags, "107")
 
 
 def gen_fp4_quantization_sm90_module() -> JitSpec:
